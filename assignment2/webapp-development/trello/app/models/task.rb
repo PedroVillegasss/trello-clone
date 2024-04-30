6bb81dd7782f43c8add68ea385583d866e2413f2
PedroVillegasss <ppvillegas@miuandes.cl>
2024-04-30 15:28:19 -0400

    seeds and queries working

--- assignment2/webapp-development/trello/app/models/task.rb
+++ assignment2/webapp-development/trello/app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :state
-  belongs_to :informer_user_id, class_name: 'User', foreign_key: 'informer_user_id'
-  belongs_to :assignee_user_id, class_name: 'User', foreign_key: 'assignee_user_id', optional: true
+  belongs_to :informer_user_id, class_name: 'User', foreign_key: 'informer_user_id_id'
+  belongs_to :assignee_user_id, class_name: 'User', foreign_key: 'assignee_user_id_id', optional: true
   belongs_to :label, optional: true
   has_many :task_comments
 
--- assignment2/webapp-development/trello/db/seeds.rb
+++ assignment2/webapp-development/trello/db/seeds.rb
@@ -8,31 +8,91 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Board.delete_all
-Label.delete_all
-State.delete_all
 TaskComment.delete_all
 Task.delete_all
-Team.delete_all
+State.delete_all
+Label.delete_all
 UserBoard.delete_all
 UserTask.delete_all
 UserTeam.delete_all
+
+Team.all.each do |team|
+  team.boards.destroy_all
+end
+
+Team.delete_all
+Board.delete_all
 User.delete_all
 
-User.create(
+
+user1 = User.create(
   name: 'Pedro Villegas',
   email: 'ppvillegas@miuandes.cl',
   password: 'despuesteladigo123',
 )
 
-User.create(
+user2 = User.create(
   name: 'Benjamin Valenzuela',
   email: 'bnvalenzuela@miuandes.cl',
   password: 'cr7stk',
 )
 
-User.create(
+user3 = User.create(
   name: 'Rodrigo Orellana',
   email: 'rrorellana@miuandes.cl',
   password: 'Rorro123',
 )
+
+team1 = Team.create(name:"grupo17")
+
+board1 = Board.create(user_id: user1.id, team_id: team1.id, name:"tablero17", color:"azul", is_public: true)
+board2 = Board.create(user_id: user2.id, name:"tablero18", color:"rojo", is_public: true)
+board3 = Board.create(user_id: user3.id, name:"tablero19", color:"verde", is_public: true)
+
+states1 = (1..4).map do |position|
+  board1.states.create(name: "estado#{position}", position: position)
+end
+
+states2 = (1..4).map do |position|
+  board2.states.create(name: "estado#{position}", position: position)
+end
+
+states3 = (1..4).map do |position|
+  board3.states.create(name: "estado#{position}", position: position)
+end
+
+5.times do |i|
+  Task.create(
+    state_id: states1.sample.id,
+    informer_user_id_id: user1.id,
+    assignee_user_id_id: [user1.id, user2.id, user3.id].sample,
+    name: "Tarea#{i+1}",
+    priority: rand(1..3),
+    description: "Esta es la descripción de la tarea #{i+1}",
+    label_id: Label.create(user_id: user1.id, board_id: board1.id, name: "label#{i+1}", color: "verde").id
+  )
+end
+
+5.times do |i|
+  Task.create(
+    state_id: states2.sample.id,
+    informer_user_id_id: user2.id,
+    assignee_user_id_id: [user1.id, user2.id, user3.id].sample,
+    name: "Tarea#{i+1}",
+    priority: rand(1..3),
+    description: "Esta es la descripción de la tarea #{i+1}",
+    label_id: Label.create(user_id: user2.id, board_id: board2.id, name: "label#{i+1}", color: "verde").id
+  )
+end
+
+5.times do |i|
+  Task.create(
+    state_id: states3.sample.id,
+    informer_user_id_id: user3.id,
+    assignee_user_id_id: [user1.id, user2.id, user3.id].sample,
+    name: "Tarea#{i+1}",
+    priority: rand(1..3),
+    description: "Esta es la descripción de la tarea #{i+1}",
+    label_id: Label.create(user_id: user3.id, board_id: board3.id, name: "label#{i+1}", color: "verde").id
+  )
+end
