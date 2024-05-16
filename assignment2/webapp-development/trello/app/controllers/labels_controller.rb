449a1dc2c0703c305c419dc9073961a468906fbc
PedroVillegasss <ppvillegas@miuandes.cl>
2024-05-16 15:00:43 -0400

    label create to a specific board working

--- assignment2/webapp-development/trello/app/controllers/labels_controller.rb
+++ assignment2/webapp-development/trello/app/controllers/labels_controller.rb
@@ -1,3 +1,20 @@
 class LabelsController < ApplicationController
+  def new
+    @label = Label.new(board_id: params[:board_id])
+  end
 
+  def create
+    @label = Label.new(label_params)
+    if @label.save!
+      redirect_to boards_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def label_params
+      params.require(:label).permit(:user_id, :board_id, :name, :color)
+    end
 end
