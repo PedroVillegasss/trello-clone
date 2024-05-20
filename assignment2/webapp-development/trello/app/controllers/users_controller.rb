e0e019f5ab5558576b62eff302415451cd8ed521
PedroVillegasss <ppvillegas@miuandes.cl>
2024-05-20 14:21:35 -0400

    users and teams index

--- assignment2/webapp-development/trello/app/controllers/users_controller.rb
+++ assignment2/webapp-development/trello/app/controllers/users_controller.rb
@@ -6,11 +6,35 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save!
-				redirect_to boards_path
+				redirect_to users_path
 		else
 				render :new
 		end
 	end
+
+	def index
+		@users = User.all
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update user_params
+				redirect_to users_path
+		else
+				render :edit
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+    @user.destroy
+		redirect_to users_path
+  end
+
 
   private
 
