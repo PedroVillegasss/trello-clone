41ab8b2b094a2858162e416b0b7dd11741fbe02a
PedroVillegasss <ppvillegas@miuandes.cl>
2024-05-21 00:54:29 -0400

    cleannn

--- assignment2/webapp-development/trello/app/controllers/labels_controller.rb
+++ assignment2/webapp-development/trello/app/controllers/labels_controller.rb
@@ -12,6 +12,31 @@ class LabelsController < ApplicationController
     end
   end
 
+	def edit
+		@label = Label.find(params[:id])
+	end
+
+	def update
+		@label = Label.find(params[:id])
+		if @label.update label_params
+				redirect_to board_path(@label.board_id)
+		else
+				render :edit
+		end
+	end
+
+	def destroy
+		@label = Label.find(params[:id])
+    @label.destroy
+		redirect_to board_path(@label.board_id)
+  end
+
+  def show
+    id = params['id']
+    @label = Label.find(id)
+    @board = @label.board
+  end
+
   private
 
     def label_params
