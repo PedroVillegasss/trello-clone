8594dffd74cbe94cebc1c2b70c3f2a33a6a1c4f8
PedroVillegasss <ppvillegas@miuandes.cl>
2024-05-21 14:01:18 -0400

    fixed a bug in board deletion

--- assignment2/webapp-development/trello/app/models/state.rb
+++ assignment2/webapp-development/trello/app/models/state.rb
@@ -1,6 +1,6 @@
 class State < ApplicationRecord
   belongs_to :board
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   validates :board_id, presence: { message: "The state must belong to a board_id (board)" }, numericality: {only_integer: true, message: "Must be a valid id"}
   validates :name, presence: { message: "The state should have a name" }
